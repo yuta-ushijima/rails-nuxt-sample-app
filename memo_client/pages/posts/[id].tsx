ed2808b3ab8addd24cd141e8ae7eebf8ec281e93
Yuta_Ushizima <contact@yuta-u.com>
2021-10-31 14:35:33 +0900

    feat: memo_apiを叩いてデータを取得・表示

---
+++ memo_client/pages/posts/[id].tsx
@@ -0,0 +1,24 @@
+const url = "http://localhost:3000/posts/3";
+const PostPage = (props) => {
+    return(
+        <div>
+            <h1>タイトル: {props.data.title}</h1>
+            <span>作成日: {props.data.created_at}</span>
+            <p>本文: {props.data.body} </p>
+        </div>
+    );
+};
+
+export async function getServerSideProps() {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data)
+    return {
+        props: {
+            data
+        },
+    };
+}
+
+export default PostPage;
